5e422ce2aefc325d31f6f6c77f6b0ee826f5e1a6
zxc87824557 <zxc90919zxc@gmail.com>
2020-07-07 11:32:06 +0800

    upload

---
+++ src/main.js
@@ -0,0 +1,25 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Photoswipe from 'vue-pswipe'
+
+import './plugins/bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// axios 預設傳送認證資訊
+axios.defaults.withCredentials = true
+
+Vue.use(VueAxios, axios)
+Vue.use(Photoswipe)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
